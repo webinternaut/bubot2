597c55511c3a21ce439d0a9a1dfcf1f66df7ce1c
webinternaut <webinternaut@gmail.com>
2022-07-11 15:14:18 -0400

    MSAL Test

--- src/authConfig.js
+++ src/authConfig.js
@@ -4,7 +4,7 @@ const msalConfig = {
     auth: {
         clientId: "be8e526e-35c4-4867-ab60-30f53b14d942",
         authority: "https://login.microsoftonline.com/86b5b2a1-375f-4c5f-885e-2b4fcbad43e6",
-        redirectUri: "https://audit.ncc1701d.xyz/"
+        redirectUri: "https://audit.ncc1701d.xyz/user/login"
     }
 };
 
