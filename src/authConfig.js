2ddb321f8dff5eaebede197ef6ad3a28a8e9f644
webinternaut <webinternaut@gmail.com>
2022-07-11 14:48:17 -0400

    MSAL Test

--- src/authConfig.js
+++ src/authConfig.js
@@ -4,13 +4,16 @@ const msalConfig = {
     auth: {
         clientId: "be8e526e-35c4-4867-ab60-30f53b14d942",
         authority: "https://login.microsoftonline.com/86b5b2a1-375f-4c5f-885e-2b4fcbad43e6",
-                redirectUri: 'https://audit.ncc1701d.xyz/user/login'
+        redirectUri: "https://audit.ncc1701d.xyz/user/login"
     }
 };
 
 const msalInstance = new msal.PublicClientApplication(msalConfig);
 
 export { msalInstance }
+
+
+
 
 
 // /*
